e2e85c39598ecab81be75854113c51d6c8172465
Emon Sheikh <sheikhemon@gmail.com>
2021-10-16 14:32:49 +0600

    spring aimated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
   return (
     <div className="App">
-
+      <animated.div style={useSpring(
+        {
+          to: { opacity: 1 },
+          reset: true,
+          reverse: flip,
+          delay: 200,
+          onRest: () => set(!flip),
+          from: { opacity: 0 }
+        })}>Spring Animated</animated.div>
     </div>
   );
 }
